a540ef6e02dbd95cf7d47906580ac0adbd3af891
ukarpenkov <truloveu@gmail.com>
2025-02-15 19:38:57 +0300

    login page

--- school/App.tsx
+++ school/App.tsx
@@ -14,65 +14,34 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <View style={styles.top}>
-        <Text style={styles.textStyle}>Мое приложение</Text>
-        <Button title="butt" />
+      <View style={styles.content}>
+        <Text>Pupppp</Text>
+        <View style={styles.form}>
+          <TextInput style={styles.input}></TextInput>
+          <TextInput style={styles.input}></TextInput>
+          <Button title="Войти"></Button>
+        </View>
+        <Text>Восстановить пароль</Text>
       </View>
-      <View
-        style={{
-          backgroundColor: "yellow",
-          alignItems: "flex-end",
-          height: 500,
-          flexDirection: "row",
-          gap: 10,
-          flexWrap: "wrap",
-        }}
-      >
-        <View
-          style={{
-            backgroundColor: "tomato",
-            width: width / 2 - 5,
-
-            height: 100,
-          }}
-        >
-          <Text>1</Text>
-        </View>
-        <View
-          style={{
-            backgroundColor: "purple",
-            width: width / 2 - 5,
-            height: 100,
-          }}
-        >
-          <Text>2</Text>
-        </View>
-        <View
-          style={{
-            backgroundColor: "green",
-            width: width / 2 - 5,
-            height: 100,
-          }}
-        >
-          <Text>3</Text>
-        </View>
-      </View>
-      <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    marginTop: 70,
+    justifyContent: "center",
+    padding: 55,
+    flex: 1,
   },
-  top: {
-    flexDirection: "row",
+  content: {
+    alignItems: "center",
+    gap: 50,
   },
-  textStyle: {
-    color: "blue",
-    fontSize: 26,
-    borderWidth: 1,
-    borderColor: "red",
+  form: {
+    alignSelf: "stretch",
+    gap: 16,
+  },
+  input: {
+    backgroundColor: "#2E2D3D",
   },
 });
