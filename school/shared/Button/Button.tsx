e47791f0475ee2d10ec1f95193137f8ea3b56732
ukarpenkov <truloveu@gmail.com>
2025-02-23 11:29:25 +0300

    animation1

--- school/shared/Button/Button.tsx
+++ school/shared/Button/Button.tsx
@@ -1,4 +1,5 @@
 import {
+  Animated,
   Pressable,
   PressableProps,
   StyleSheet,
@@ -8,11 +9,30 @@ import {
 import { Colors, Fonts, Radius } from "../tokens";
 
 export function Button({ text, ...props }: PressableProps & { text: string }) {
+  const animatedValue = new Animated.ValueXY({
+    x: 0,
+    y: 0,
+  });
+  Animated.timing(animatedValue, {
+    toValue: {
+      x: 100,
+      y: 100,
+    },
+    useNativeDriver: true,
+  }).start();
   return (
     <Pressable {...props}>
-      <View style={styles.button}>
+      <Animated.View
+        style={{
+          ...styles.button,
+          transform: [
+            { translateX: animatedValue.x },
+            { translateY: animatedValue.y },
+          ],
+        }}
+      >
         <Text style={styles.text}>{text}</Text>
-      </View>
+      </Animated.View>
     </Pressable>
   );
 }
