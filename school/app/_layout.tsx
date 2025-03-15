619809c9c080bbbd6df3d8c4bd16971e5acb8f4d
ukarpenkov <truloveu@gmail.com>
2025-03-15 13:19:46 +0300

    Enhance RootLayout with custom screen options and add Restore screen

--- school/app/_layout.tsx
+++ school/app/_layout.tsx
@@ -1,6 +1,25 @@
 import { Navigator, Slot, Stack, Tabs } from 'expo-router'
 import { Text } from 'react-native'
+import { Colors } from '../shared/tokens'
 
 export default function RootLayout() {
-    return <Stack />
+    return (
+        <Stack
+            screenOptions={{
+                statusBarBackgroundColor: Colors.black,
+                contentStyle: {
+                    backgroundColor: Colors.black,
+                },
+            }}
+        >
+            <Stack.Screen name="index" />
+            <Stack.Screen
+                name="restore"
+                options={{
+                    presentation: 'modal',
+                    headerShown: false,
+                }}
+            />
+        </Stack>
+    )
 }
--- school/app/restore.tsx
+++ school/app/restore.tsx
@@ -1,5 +1,6 @@
 import { Link } from 'expo-router'
 import { Text, View } from 'react-native'
+import { Colors } from '../shared/tokens'
 
 export default function Restore() {
     return (
