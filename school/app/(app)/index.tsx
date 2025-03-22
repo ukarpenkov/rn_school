9010270871fe35ef0c97169eb350ef72e4d2be9d
ukarpenkov <truloveu@gmail.com>
2025-03-22 14:07:00 +0300

    Add AsyncStorage integration for demo data in MyCourses component

--- school/app/(app)/index.tsx
+++ school/app/(app)/index.tsx
@@ -2,9 +2,20 @@ import { Text, View } from 'react-native'
 import { Colors } from '../../shared/tokens'
 import { useAtom } from 'jotai'
 import { profileAtom } from '../../entities/user/model/user.state'
+import { useEffect } from 'react'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
 export default function MyCourses() {
     const [profile] = useAtom(profileAtom)
+
+    useEffect(() => {
+        AsyncStorage.setItem('demo', 'Test').then(async () => {
+            console.log(await AsyncStorage.getAllKeys())
+            console.log(await AsyncStorage.getItem('demo'))
+            console.log(await AsyncStorage.removeItem('demo'))
+            console.log(await AsyncStorage.getItem('demo'))
+        })
+    }, [profile])
     return (
         <View>
             <Text>{profile.profile?.name}</Text>
