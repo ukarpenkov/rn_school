826fbd4786d7dd2c9ba6787c1d286fd0ad323352
ukarpenkov <truloveu@gmail.com>
2025-03-30 13:55:25 +0300

    Enhance AppLayout with custom header styles and add MenuIcon component

--- school/app/(app)/_layout.tsx
+++ school/app/(app)/_layout.tsx
@@ -2,6 +2,9 @@ import { Stack, SplashScreen, Redirect } from 'expo-router'
 import { Drawer } from 'expo-router/drawer'
 import { useAtomValue } from 'jotai'
 import { authAtom } from '../../entities/auth/model/auth.state'
+import { Colors, Fonts } from '../../shared/tokens'
+import { Text } from 'react-native'
+import MenuIcon from '../../assets/icons/menu'
 
 export default function AppLayout() {
     const { access_token } = useAtomValue(authAtom)
@@ -9,8 +12,28 @@ export default function AppLayout() {
         return <Redirect href="/login" />
     }
     return (
-        <Drawer>
-            <Drawer.Screen name="index" />
+        <Drawer
+            screenOptions={({ navigation }) => ({
+                headerStyle: {
+                    backgroundColor: Colors.blackLight,
+                    shadowColor: Colors.blackLight,
+                    shadowOpacity: 0,
+                },
+                headerLeft: () => {
+                    return <MenuIcon />
+                },
+                headerTitleStyle: {
+                    color: Colors.white,
+                    fontFamily: 'FiraSans',
+                    fontSize: Fonts.f20,
+                },
+                headerTitleAlign: 'center',
+                sceneStyle: {
+                    backgroundColor: Colors.black,
+                },
+            })}
+        >
+            <Drawer.Screen name="index" options={{ title: 'Мои курсы' }} />
         </Drawer>
     )
 }
--- school/shared/tokens.ts
+++ school/shared/tokens.ts
@@ -1,25 +1,27 @@
 export const Colors = {
-  black: "#16171D",
-  gray: "#AFB2BF",
-  violetDark: "#2E2D3D",
-  primary: "#6C38CC",
-  primaryHover: "#452481",
-  link: "#A97BFF",
-  white: "#FAFAFA",
-  red: "#CC384E",
-};
+    black: '#16171D',
+    blackLight: '#1E1F29',
+    gray: '#AFB2BF',
+    violetDark: '#2E2D3D',
+    primary: '#6C38CC',
+    primaryHover: '#452481',
+    link: '#A97BFF',
+    white: '#FAFAFA',
+    red: '#CC384E',
+}
 
 export const Gaps = {
-  g16: 16,
-  g50: 50,
-};
+    g16: 16,
+    g50: 50,
+}
 
 export const Radius = {
-  r10: 10,
-};
+    r10: 10,
+}
 export const Fonts = {
-  f16: 16,
-  f18: 18,
-  regular: 'FiraSans',
-  semibold: 'FiraSansSemiBold',
-};
+    f16: 16,
+    f18: 18,
+    f20: 20,
+    regular: 'FiraSans',
+    semibold: 'FiraSansSemiBold',
+}
