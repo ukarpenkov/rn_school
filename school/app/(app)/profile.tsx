b36f0d8c00c6aca56c143323f93b3ce24996acf3
ukarpenkov <truloveu@gmail.com>
2025-04-14 21:33:06 +0300

    Add image picker functionality to Profile screen and update dependencies

--- school/app/(app)/profile.tsx
+++ school/app/(app)/profile.tsx
@@ -1,12 +1,26 @@
 import { Text, View } from 'react-native'
 import { useSetAtom } from 'jotai'
 import { logoutAtom } from '../../entities/auth/model/auth.state'
+import { useState } from 'react'
+import * as ImagePicker from 'expo-image-picker'
+import { Button } from '../../shared/Button/Button'
 
 export default function Profile() {
-    const logout = useSetAtom(logoutAtom)
+    const [image, setImage] = useState(null)
+
+    const pickAvatar = async () => {
+        const result = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ['images', 'videos'],
+            allowsEditing: true,
+            aspect: [1, 1],
+            quality: 0.5,
+        })
+        console.log(result)
+    }
     return (
         <View>
             <Text>PROFILE</Text>
+            <Button text="Выбрать изображение" onPress={pickAvatar}></Button>
         </View>
     )
 }
