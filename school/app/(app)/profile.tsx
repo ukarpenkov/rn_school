e118aae6bb93d096cb893ecb4e0ff906bb80a0f6
ukarpenkov <truloveu@gmail.com>
2025-04-18 14:42:35 +0300

    Enhance Profile screen: add media library permissions handling, update avatar selection methods, and improve button labels for image capture and selection.

--- school/app/(app)/profile.tsx
+++ school/app/(app)/profile.tsx
@@ -1,4 +1,4 @@
-import { Alert, Text, View } from 'react-native'
+import { Alert, Image, Text, View } from 'react-native'
 import { useSetAtom } from 'jotai'
 import { logoutAtom } from '../../entities/auth/model/auth.state'
 import { useState } from 'react'
@@ -6,9 +6,11 @@ import * as ImagePicker from 'expo-image-picker'
 import { Button } from '../../shared/Button/Button'
 
 export default function Profile() {
-    const [image, setImage] = useState(null)
+    const [image, setImage] = useState<string | null>(null)
     const [cameraPermissions, requestCameraPermission] =
         ImagePicker.useCameraPermissions()
+    const [libraryPermissions, requestLibraryPermission] =
+        ImagePicker.useMediaLibraryPermissions()
 
     const verifyPermissions = async () => {
         if (
@@ -27,9 +29,42 @@ export default function Profile() {
         }
         return true
     }
+    const verifyMediaPermissions = async () => {
+        if (
+            libraryPermissions?.status ===
+            ImagePicker.PermissionStatus.UNDETERMINED
+        ) {
+            const res = await requestLibraryPermission()
+            return res.granted
+        }
+        if (cameraPermissions?.status === ImagePicker.PermissionStatus.DENIED) {
+            Alert.alert(
+                'Insufficient permissions!',
+                'You need to grant galery permissions to use this app.'
+            )
+            return false
+        }
+        return true
+    }
 
-    const pickAvatar = async () => {
+    const captureAvatar = async () => {
         const isPermissionsGranted = await verifyPermissions()
+        if (!isPermissionsGranted) {
+            return
+        }
+        const result = await ImagePicker.launchCameraAsync({
+            mediaTypes: ['images', 'videos'],
+            allowsEditing: true,
+            aspect: [1, 1],
+            quality: 0.5,
+        })
+        if (!result.assets) {
+            return
+        }
+        setImage(result.assets[0].uri)
+    }
+    const pickAvatar = async () => {
+        const isPermissionsGranted = await verifyMediaPermissions()
         if (!isPermissionsGranted) {
             return
         }
@@ -39,12 +74,25 @@ export default function Profile() {
             aspect: [1, 1],
             quality: 0.5,
         })
-        console.log(result)
+        if (!result.assets) {
+            return
+        }
+        setImage(result.assets[0].uri)
     }
     return (
         <View>
             <Text>PROFILE</Text>
-            <Button text="Выбрать изображение" onPress={pickAvatar}></Button>
+            <Button text="Снять изображение" onPress={captureAvatar}></Button>
+            <Button text="Выбрать из галереи" onPress={pickAvatar}></Button>
+            {image && (
+                <Image
+                    source={{
+                        uri: image,
+                        width: 200,
+                        height: 200,
+                    }}
+                />
+            )}
         </View>
     )
 }
