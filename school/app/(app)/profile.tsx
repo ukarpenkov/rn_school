588b0695b79c2675d79726924adb9f8b6854596b
ukarpenkov <truloveu@gmail.com>
2025-04-18 14:05:25 +0300

    Add camera permissions handling to Profile screen for image selection

--- school/app/(app)/profile.tsx
+++ school/app/(app)/profile.tsx
@@ -1,4 +1,4 @@
-import { Text, View } from 'react-native'
+import { Alert, Text, View } from 'react-native'
 import { useSetAtom } from 'jotai'
 import { logoutAtom } from '../../entities/auth/model/auth.state'
 import { useState } from 'react'
@@ -7,8 +7,32 @@ import { Button } from '../../shared/Button/Button'
 
 export default function Profile() {
     const [image, setImage] = useState(null)
+    const [cameraPermissions, requestCameraPermission] =
+        ImagePicker.useCameraPermissions()
+
+    const verifyPermissions = async () => {
+        if (
+            cameraPermissions?.status ===
+            ImagePicker.PermissionStatus.UNDETERMINED
+        ) {
+            const res = await requestCameraPermission()
+            return res.granted
+        }
+        if (cameraPermissions?.status === ImagePicker.PermissionStatus.DENIED) {
+            Alert.alert(
+                'Insufficient permissions!',
+                'You need to grant camera permissions to use this app.'
+            )
+            return false
+        }
+        return true
+    }
 
     const pickAvatar = async () => {
+        const isPermissionsGranted = await verifyPermissions()
+        if (!isPermissionsGranted) {
+            return
+        }
         const result = await ImagePicker.launchImageLibraryAsync({
             mediaTypes: ['images', 'videos'],
             allowsEditing: true,
