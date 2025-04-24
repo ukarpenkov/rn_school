45c2bbee55c6f4672501190a098dfa9ca4e7cb25
ukarpenkov <truloveu@gmail.com>
2025-04-24 17:29:48 +0300

    Update avatar image dimensions to 100x100 for improved display

--- school/app/(app)/profile.tsx
+++ school/app/(app)/profile.tsx
@@ -49,8 +49,8 @@ export default function Profile() {
                 <Image
                     source={{
                         uri: image,
-                        width: 200,
-                        height: 200,
+                        width: 100,
+                        height: 100,
                     }}
                 />
             ) : (
